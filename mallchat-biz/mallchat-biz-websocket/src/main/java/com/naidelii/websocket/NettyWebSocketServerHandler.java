8b59e32222cbbc1ae33f9642bf910811b858d1a2
naidelii <naidelii@qq.com>
2023-11-08 21:34:53 +0800

    feat：websocket模块

---
+++ mallchat-biz/mallchat-biz-websocket/src/main/java/com/naidelii/websocket/NettyWebSocketServerHandler.java
@@ -0,0 +1,22 @@
+package com.naidelii.websocket;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author naidelii
+ */
+@Slf4j
+@Sharable
+public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext context, TextWebSocketFrame message) {
+        // 获取到的新消息
+        String text = message.text();
+        log.info("接收到的新消息：{}", text);
+    }
+}
---
+++ mallchat-chat-server/src/main/java/com/naidelii/chat/MallChatServerApplication.java
@@ -0,0 +1,14 @@
+package com.naidelii.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author naidelii
+ */
+@SpringBootApplication
+public class MallChatServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallChatServerApplication.class, args);
+    }
+}
